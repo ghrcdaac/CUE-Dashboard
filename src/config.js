9894309d50d1ec57d20443987e631627074446ae
Navaneeth R Selvaraj <navaneeth026@gmail.com>
2025-08-03 20:53:27 -0500

    adding UAT info

--- src/config.js
+++ src/config.js
@@ -1,8 +1,19 @@
 // src/config.js
 export const config = {
-    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "https://upload.sit.earthdata.nasa.gov/api", 
+    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "https://upload.uat.earthdata.nasa.gov/api", 
     version: "0.0.20",
-    cognitoUserPoolId: "us-west-2_P9iA4SzlA", 
-    cognitoClientId: "79gtmmmfqupli6li25aar2p4u9"
+    cognitoUserPoolId: "us-west-2_x9nYiAMKS", 
+    cognitoClientId: "44cr4134perjrvkgm437363fko"
 
 };
+
+
+// SIT
+// cognitoUserPoolId: "us-west-2_P9iA4SzlA", 
+// cognitoClientId: "79gtmmmfqupli6li25aar2p4u9"
+// 
+
+
+// UAT
+// cognitoUserPoolId: "us-west-2_x9nYiAMKS", 
+// cognitoClientId: "44cr4134perjrvkgm437363fko"
