844dd594f351e196ecff71d14678b93ce2d2fd51
jananir-21 <jr0216@uah.edu>
2025-12-01 13:45:24 -0600

    Home and store data changes

--- src/app/reducers/dataCacheSlice.js
+++ src/app/reducers/dataCacheSlice.js
@@ -34,14 +34,14 @@ export const fetchUsers = createAsyncThunk(
   }
 );
 
-export const fetchCollections = createAsyncThunk( 
+export const fetchCollections = createAsyncThunk(
   'dataCache/fetchCollections', async ({ page, pageSize }, { rejectWithValue }) => {
-     try { 
-      const response = await collectionApi.listCollections(page, pageSize); 
-      return response; // response = { collections, page, page_size, total } 
-    } 
-    catch (error) { 
-      return rejectWithValue(parseApiError(error)); } 
+     try {
+      const response = await collectionApi.listCollections(page, pageSize);
+      return response; // response = { collections, page, page_size, total }
+    }
+    catch (error) {
+      return rejectWithValue(parseApiError(error)); }
   } );
 
 export const fetchRoles = createAsyncThunk(
